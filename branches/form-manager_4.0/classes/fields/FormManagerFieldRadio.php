edfaa923f835883745e66462dc686877a776f93f
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2011-09-30 20:28:31 +0000

    Исправлены ссылки на документацию в комментариях

--- branches/form-manager_4.0/classes/fields/FormManagerFieldRadio.php
+++ branches/form-manager_4.0/classes/fields/FormManagerFieldRadio.php
@@ -8,7 +8,7 @@
  * @author		Peter S. Gribanov <info@peter-gribanov.ru>
  * @version		4.0 SVN: $Revision$
  * @since		$Date$
- * @link		http://peter-gribanov.ru/open-source/form-manager_4.0/
+ * @link		http://peter-gribanov.ru/open-source/form-manager/4.0/
  * @copyright	(c) 2008 by Peter S. Gribanov
  * @license		http://peter-gribanov.ru/license	GNU GPL Version 3
  */
