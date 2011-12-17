9263db7c72df3b33a953e2ebc8e49cb9b22bbc15
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2011-12-17 15:27:45 +0000

    изменил родителя интерфейса поля и добавил интерфейс коллекции

---
+++ FormManager/Interfaces/Model/Collection.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * FormManager package
+ * 
+ * @package   FormManager
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @version   4.0 SVN: $Revision: 120 $
+ * @since     $Date: 2011-12-15 08:31:54 +0400 (Thu, 15 Dec 2011) $
+ * @link      http://peter-gribanov.ru/open-source/form-manager/4.0/
+ * @copyright 2008 by Peter Gribanov
+ * @license   http://peter-gribanov.ru/license	GNU GPL Version 3
+ */
+
+/**
+ * Интерфейс модели коллекции элиментов
+ * 
+ * @package FormManager\Interfaces\Model
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+interface FormManager_Interfaces_Model_Collection extends FormManager_Interfaces_Model_Element {
+
+}
