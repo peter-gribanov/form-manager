33da123827aaf4d48fd366245f092761e6951003
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2011-12-17 19:49:21 +0000

    реализовал для фильтра дата поддержку формата даты

--- FormManager/Filter/Field/Date.php
+++ FormManager/Filter/Field/Date.php
@@ -23,8 +23,19 @@ class FormManager_Filter_Field_Data extends FormManager_Filter_Field_Abstract {
 	 * Проверяет поле
 	 */
 	public function check(){
-		// @todo формат даты может измениться и это надо учитывать
-		if ( $this->field->getValue() != '' && !preg_match('/^\d\d\.\d\d\.\d{4}$/', $this->field->getValue()) ) {
+		if (isset($this->options['format']) && trim($this->options['format'])) {
+			$format = trim($this->options['format']);
+			if (substr_count($format, 'DD') == 1 && substr_count($format, 'MM')
+				&& (substr_count($format, 'YY') == 1 || substr_count($format, 'YYYY'))) {
+
+				$format = str_replace(array('DD','MM','YY'), '\d\d', $format);
+				$format = str_replace('.', '\.', $format);
+				$format = '/^'.$format.'$/';
+			}
+		} else {
+			$format = '/^\d\d\.\d\d\.\d{4}$/'; // DD.MM.YYYY
+		}
+		if ($this->element->getValue() != '' && !preg_match($format, $this->element->getValue())) {
 			$this->trigger('date');
 		}
 	}
