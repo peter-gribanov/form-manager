e7bc7b1c66c6f1d4fc5a0331045201655295abe1
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2011-12-19 18:07:55 +0000

    исправил ошибку добавив методам __call() второй параметр

--- FormManager/Filter/Factory.php
+++ FormManager/Filter/Factory.php
@@ -23,10 +23,11 @@ final class FormManager_Filter_Factory {
 	 * Возвращает новый фильтр
 	 * 
 	 * @param string $method Вызываемый метод
+	 * @param array  $args   Параметры метода
 	 * 
 	 * @return FormManager_Interfaces_Filter
 	 */
-	public function __call($method) {
+	public function __call($method, $args) {
 		$obj = $this->get($method);
 		// TODO реализовать добавление метода в класс
 		return $obj;
