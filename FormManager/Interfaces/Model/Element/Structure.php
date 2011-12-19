8d566cd0908f7fa60f5d6f5d47a0134e2b7f69ce
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2011-12-19 19:36:42 +0000

    добавил уровень пакета для интерфейсов элемента

--- FormManager/Interfaces/Model/Element/Structure.php
+++ FormManager/Interfaces/Model/Element/Structure.php
@@ -14,7 +14,7 @@
 /**
  * Интерфейс модели элемента формулирующая композиционную структуру
  * 
- * @package FormManager\Interfaces
+ * @package FormManager\Interfaces\Element
  * @author  Peter Gribanov <info@peter-gribanov.ru>
  */
 interface FormManager_Interfaces_Model_Element_Structure extends IteratorAggregate {
