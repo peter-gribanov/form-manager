9f0a04261d5874cdd445e9a6c47c853f959f422e
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2011-12-22 17:53:11 +0000

    объеденил обратно интерфейс FormManager_Interfaces_Model_Element в единое целое

--- FormManager/Interfaces/Model/Element.php
+++ FormManager/Interfaces/Model/Element.php
@@ -17,9 +17,168 @@
  * @package FormManager\Interfaces\Model
  * @author  Peter Gribanov <info@peter-gribanov.ru>
  */
-interface FormManager_Interfaces_Model_Element extends
-	FormManager_Interfaces_Model_Element_Descriptor,
-	FormManager_Interfaces_Model_Element_Structure,
-	FormManager_Interfaces_Model_Element_Functional {
+interface FormManager_Interfaces_Model_Element extends IteratorAggregate, FormManager_Interfaces_Model {
+
+	/**
+	 * TODO добавить описание
+	 * 
+	 * @param FormManager_Interfaces_Model_Element $element
+	 * 
+	 * @return FormManager_Interfaces_Model_Element
+	 */
+	public function add(FormManager_Interfaces_Model_Element $element);
+
+	/**
+	 * Разбирает строку запроса и добавляет скрытые поля с переменными из запроса
+	 * Пример строки запроса: a=foo&b=bar
+	 * 
+	 * @param string $query
+	 */
+	public function addByQuery($query);
+
+	/**
+	 * TODO добавить описание
+	 * 
+	 * @param FormManager_Interfaces_Model_Element $element
+	 */
+	public function setParent(FormManager_Interfaces_Model_Element $element);
+
+	/**
+	 * TODO добавить описание
+	 * 
+	 * @return FormManager_Interfaces_Model_Element
+	 */
+	public function getParent();
+
+	/**
+	 * TODO добавить описание
+	 * 
+	 * @param FormManager_Interfaces_Model_Element $element
+	 */
+	public function setRoot(FormManager_Interfaces_Model_Element $element);
+
+	/**
+	 * TODO добавить описание
+	 * 
+	 * @return array
+	 */
+	public function getChilds();
+
+	/**
+	 * TODO добавить описание
+	 * 
+	 * @param string $name
+	 * 
+	 * @return FormManager_Interfaces_Model_Element|boolean
+	 */
+	public function getChild($name);
+
+	/**
+	 * TODO добавить описание
+	 * 
+	 * @param string $name
+	 * 
+	 * @return boolean
+	 */
+	public function isAdded($name);
+
+	/**
+	 * TODO добавить описание
+	 * 
+	 * @param string $name
+	 * 
+	 * @return boolean
+	 */
+	public function remove($name);
+
+	/**
+	 * Очищает список элементов
+	 */
+	public function clear();
+
+	/**
+	 * Проверяет пуста ли коллекция
+	 * 
+	 * @return boolean
+	 */
+	public function isEmpty();
+
+	/**
+	 * Устанавливает фильтр для поля
+	 * 
+	 * @param FormManager_Interfaces_Filter $filter Объект фильтра
+	 */
+	public function setFilter(FormManager_Interfaces_Filter $filter);
+
+	/**
+	 * Добавляет декоратор
+	 * 
+	 * @param string $name  Название декораторв
+	 * @param string $value Значение декоратора
+	 */
+//	public function setDecorator($name, $value = null);
+
+	/**
+	 * Возвращает декоратор для указанного названия
+	 * 
+	 * @param string $name
+	 * 
+	 * @return string
+	 */
+//	public function getDecorator($name);
+
+	/**
+	 * Проверяет на валидность элемент
+	 * 
+	 * @return boolean
+	 */
+//	public function isValid();
+
+	/**
+	 * TODO добавить описание
+	 * 
+	 * @param string $name
+	 */
+	public function setName($name);
+
+	/**
+	 * TODO добавить описание
+	 * 
+	 * @return string
+	 */
+	public function getName();
+
+	/**
+	 * TODO добавить описание
+	 * 
+	 * @return array
+	 */
+	public function getNamesList();
+
+	/**
+	 * TODO добавить описание
+	 * 
+	 * @param string $comment
+	 */
+	public function setComment($comment);
+
+	/**
+	 * TODO добавить описание
+	 * 
+	 * @param string $title
+	 */
+	public function setTitle($title);
+
+	/**
+	 * Устанавливает флаг что есть поля обязательные для заполнения
+	 */
+	public function required();
+
+	/**
+	 * Проверяет есть ли поля обязательные для заполнения
+	 * 
+	 * @return boolean
+	 */
+	public function isRequired();
 
 }
