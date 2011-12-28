faa2b4d5bdb371be369a5543e901b2d4a16a924b
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2011-12-28 20:06:14 +0000

    добавил плагин jQueryDatePicker без реализации

---
+++ branches/plugins/4.0/jquerydatepicker_1.0/templates/.default/fields/jquerydatepicker/template.php
@@ -0,0 +1,17 @@
+<?
+/**
+ * FormManager package
+ * 
+ * @package    FormManager
+ * @subpackage jQueryDatePicker
+ * @author     Peter Gribanov <info@peter-gribanov.ru>
+ * @version    4.0 SVN: $Revision: 169 $
+ * @since      $Date: 2011-12-28 23:10:31 +0400 (Wed, 28 Dec 2011) $
+ * @link       http://peter-gribanov.ru/open-source/form-manager/4.0/
+ * @copyright  2008 by Peter Gribanov
+ * @license    http://peter-gribanov.ru/license	GNU GPL Version 3
+ */
+?><input type="text" name="<?=$this->getName()?>" value="<?=$this->getValue()?>"<?=
+($params['id'] ? ' id="'.$params['id'].'"' : '')?><?=
+($params['disabled'] ? ' disabled="disabled"' : '')
+?> class="<?=($params['class'] ? $params['class'].' ' : '')?>datepicker" />
