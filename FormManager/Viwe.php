c721a2d1fd73eea1b6c4c0c89c7341492104ce56
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2011-12-28 21:14:44 +0000

    сделал базовую реализачию вида формы

--- FormManager/Viwe.php
+++ FormManager/Viwe.php
@@ -19,6 +19,79 @@
  */
 class FormManager_Viwe {
 
+	/**
+	 * Название шаблона
+	 * 
+	 * @var string
+	 */
+	private $template = self::DEFAULT_TEMPLATE;
+
+
+	/**
+	 * Название шаблона по умолчанию
+	 * 
+	 * @var string
+	 */
+	const DEFAULT_TEMPLATE = '.default';
+
 	// TODO требуется реализация
 
+	/**
+	 * Устанавливает название шаблона
+	 * 
+	 * @throws FormManager_Exception
+	 * 
+	 * @param string $template Название шаблона
+	 */
+	public function setTemplate($template) {
+		if (!is_string($template) || !trim($template)) {
+			// TODO описать исключение
+			throw new FormManager_Exception();
+		}
+		if (strpos($template, DIRECTORY_SEPARATOR) !== false) {
+			// TODO описать исключение
+			throw new FormManager_Exception();
+		}
+		if (!is_dir(FORM_MANAGER_PATH.'templates/'.$template)) {
+			// TODO описать исключение
+			throw new FormManager_Exception();
+		}
+		$this->template = $template;
+	}
+
+	/**
+	 * Хелпер возвращающий языковое сообщение
+	 * 
+	 * @see FormManager_Language::getMessage()
+	 * 
+	 * @param string $id     Id сообщения
+	 * @param array  $params Параметры сообщения
+	 * 
+	 * @return string
+	 */
+	public function lang($id, array $params = array()) {
+		return FormManager_Language::getMessage($id, $params);
+	}
+
+	/**
+	 * Хелпер возвращающий абсолютный путь
+	 * 
+	 * @param string $path Путь отнасительно корня шаблона
+	 * 
+	 * @return string
+	 */
+	public function path($path) {
+		return FORM_MANAGER_HTTP_PATH.'templates/'.$this->template.'/'.$path;
+	}
+
+	/**
+	 * Рисует форму
+	 * 
+	 * @param FormManager_Model_Form $form Форма
+	 */
+	public function drow(FormManager_Model_Form $form) {
+		// TODO требуется реализация
+		$vars = $form->export();
+		include $this->template.'template.php';
+	}
 }
