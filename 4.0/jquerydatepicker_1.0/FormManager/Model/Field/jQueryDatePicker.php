60126ea15fb37249f0dabbaece9309768eb7b9d2
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2011-12-28 20:06:14 +0000

    добавил плагин jQueryDatePicker без реализации

---
+++ 4.0/jquerydatepicker_1.0/FormManager/Model/Field/jQueryDatePicker.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * FormManager package
+ * 
+ * @package    FormManager
+ * @subpackage jQueryDatePicker
+ * @author     Peter Gribanov <info@peter-gribanov.ru>
+ * @version    4.0 SVN: $Revision: 169 $
+ * @since      $Date: 2011-12-28 23:10:31 +0400 (Wed, 28 Dec 2011) $
+ * @link       http://peter-gribanov.ru/open-source/form-manager/4.0/
+ * @copyright  2008 by Peter Gribanov
+ * @license    http://peter-gribanov.ru/license	GNU GPL Version 3
+ */
+
+/**
+ * Класс описывает поле ввода даты
+ * 
+ * @package FormManager\Model\Field
+ * @author  Peter S. Gribanov <info@peter-gribanov.ru>
+ */
+class FormManager_Model_Field_jQueryDatePicker extends FormManager_Model_Field_SelectMulti {
+
+	/**
+	 * Конструктор
+	 */
+	public function __construct() {
+		// TODO реализовать базовую инициализацию элемента
+	}
+
+}
---
+++ 4.0/jquerydatepicker_1.0/templates/.default/fields/jquerydatepicker/.parameters.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * FormManager package
+ * 
+ * @package    FormManager
+ * @subpackage jQueryDatePicker
+ * @author     Peter Gribanov <info@peter-gribanov.ru>
+ * @version    4.0 SVN: $Revision: 169 $
+ * @since      $Date: 2011-12-28 23:10:31 +0400 (Wed, 28 Dec 2011) $
+ * @link       http://peter-gribanov.ru/open-source/form-manager/4.0/
+ * @copyright  2008 by Peter Gribanov
+ * @license    http://peter-gribanov.ru/license	GNU GPL Version 3
+ */
+
+return array(
+	'id'       => '',
+	'class'    => '',
+	'disabled' => false
+);
