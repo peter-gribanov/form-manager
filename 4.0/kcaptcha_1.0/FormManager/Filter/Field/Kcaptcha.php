604a713cf7e1052450158ba02284ccfe8cbe8447
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2011-12-28 20:09:53 +0000

    добавил в описание название subpackage-а

--- 4.0/kcaptcha_1.0/FormManager/Filter/Field/Kcaptcha.php
+++ 4.0/kcaptcha_1.0/FormManager/Filter/Field/Kcaptcha.php
@@ -2,13 +2,14 @@
 /**
  * FormManager package
  * 
- * @package   FormManager
- * @author    Peter Gribanov <info@peter-gribanov.ru>
- * @version   4.0 SVN: $Revision: 94 $
- * @since     $Date: 2011-11-19 17:23:19 +0300 (Sat, 19 Nov 2011) $
- * @link      http://peter-gribanov.ru/open-source/form-manager/4.0/
- * @copyright 2008 by Peter Gribanov
- * @license   http://peter-gribanov.ru/license	GNU GPL Version 3
+ * @package    FormManager
+ * @subpackage Kcaptcha
+ * @author     Peter Gribanov <info@peter-gribanov.ru>
+ * @version    4.0 SVN: $Revision: 94 $
+ * @since      $Date: 2011-11-19 17:23:19 +0300 (Sat, 19 Nov 2011) $
+ * @link       http://peter-gribanov.ru/open-source/form-manager/4.0/
+ * @copyright  2008 by Peter Gribanov
+ * @license    http://peter-gribanov.ru/license	GNU GPL Version 3
  */
 
 /**
