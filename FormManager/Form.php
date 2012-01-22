fa9ded8007b82b1bf193da13b55a934ee7bb5c55
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2012-01-22 17:42:11 +0000

    дополнил инициализацию формы. убрал из формы все лишнее

--- FormManager/Form.php
+++ FormManager/Form.php
@@ -14,18 +14,29 @@
 /**
  * Класс описывает форму и позволяет ее динамически составлять
  * 
- * TODO форма это частный случай коллекции
+ * Форма это частный случай коллекции
+ * TODO нужно ввести понятие подписания формы
  * 
  * @package FormManager
  * @author  Peter S. Gribanov <info@peter-gribanov.ru>
  */
-class FormManager_Form extends FormManager_Element implements FormManager_Collection_Interface {
+class FormManager_Form extends FormManager_Collection_Abstract {
 
 	/**
 	 * Конструктор
+	 * 
+	 * @param string|null $name     Имя формы
+	 * @param string|null $elements Список элементов
+	 * @param string|null $label    Подпись формы
+	 * @param string      $method   Метод отправки формы
 	 */
-	public function __construct() {
-		$this->setDecorator('action', '');
+	public function __construct($name = null, $elements = null, $label = null, $method = 'post') {
+		parent::__construct($name, $elements);
+		$this
+			->addDecorator('action', '')
+			->addDecorator('method', $method)
+			->addDecorator('id', new FormManager_Decorator_ElementId('ele', '-'))/*
+			->addDecorator('controls', 'submit')*/;
 	}
 
 	/**
@@ -33,111 +44,11 @@ class FormManager_Form extends FormManager_Element implements FormManager_Collec
 	 * 
 	 * @throws FormManager_Exceptions_NoAction
 	 * 
-	 * @param FormManager_Element_Interface $element
+	 * @param FormManager_Collection_Interface $parent
 	 */
-	public function setParent(FormManager_Element_Interface $element) {
+	public function setParent(FormManager_Collection_Interface $parent) {
 		// TODO описать исключение
 		throw new FormManager_Exceptions_NoAction();
 	}
-
-	/**
-	 * TODO добавить описание
-	 * 
-	 * @throws FormManager_Exceptions_NoAction
-	 * 
-	 * @param FormManager_Element_Interface $element
-	 */
-	public function setRoot(FormManager_Element_Interface $element) {
-		// TODO описать исключение
-		throw new FormManager_Exceptions_NoAction();
-	}
-
-	/**
-	 * TODO добавить описание
-	 * 
-	 * @return FormManager_Form
-	 */
-	protected function getRoot() {
-		// может возникнуть бесконечная рекурсия
-		return $this;
-	}
-
-	/**
-	 * Возвращает значение указанное пользователем
-	 * 
-	 * @param string $name
-	 * 
-	 * @return string
-	 *//*
-	public function &getSentValue($name) {
-		// TODO убрать отсюда
-		return $GLOBALS['_POST'][$name];
-	}*/
-
-	/**
-	 * Очищает отправленные данные
-	 *//*
-	public function clearSentValues() {
-		// TODO убрать отсюда
-		$method = '_POST';
-		unset($GLOBALS[$method]);
-		// для корректной работы методов isAlreadySent
-		// создается не пустой массив
-		$GLOBALS[$method] = array(0);
-//		$this->inputs = & $GLOBALS[$method];
-	}*/
-
-	/**
-	 * Форма уже отправлена
-	 * 
-	 * @return boolean
-	 *//*
-	public function isAlreadySent() {
-		// TODO убрать в фильтры
-		if ( !isset($_SERVER['HTTP_REFERER'])
-			|| !count($GLOBALS['_'.$this->options['method']])
-			// должен быть установлен уникальный ключ,
-			// но он не обнаружен в полученных данных
-			|| ($this->options['method'] == 'GET'
-				&& $this->getSentValue('unique_key_already_sent')
-					!=='4ab24a54898e90ea76f23afc36a81819') ) {
-
-			return false;
-		}
-
-		// получение тикущего хотса
-		$current = ($_SERVER['SERVER_PROTOCOL'][4]=='S' ? 'https' : 'http').'://'
-			.$_SERVER['HTTP_HOST'].'/';
-		// разрешен прием форм в пределах одного хоста
-		return strpos($_SERVER['HTTP_REFERER'], $current)===0;
-	}*/
-
-	/**
-	 * Устанавливает метод передачи данных
-	 *
-	 * @throws FormManager_Exceptions_Form
-	 * 
-	 * @param string $method
-	 * 
-	 * @return FormManager_Form
-	 *//*
-	public function setMethod($method) {
-		$method = strtoupper($method);
-		if ($method != 'POST' && $method != 'GET') {
-			throw new FormManager_Exceptions_Form('Form method must be POST or GET');
-		}
-
-		$this->options['method'] = $method;
-		// добавление скрытого поля
-		if ($method == 'GET' && !$this->getRoot()->isAddedByName('unique_key_already_sent')) {
-			// TODO должен накладываться фильтр
-			$field = new FormManager_Field_Hidden();
-			$field->setName('unique_key_already_sent');
-			$field->setDefaultValue('4ab24a54898e90ea76f23afc36a81819');
-			$this->getRoot()->add($field);
-		}
-
-		return $this;
-	}*/
 
 }
