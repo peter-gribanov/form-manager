4e34b492de03b08816059243d6d11cc138a17068
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2012-01-22 17:39:46 +0000

    расширил базовую инициализацию текстового поля и выпадающего списка

--- FormManager/Field/Text.php
+++ FormManager/Field/Text.php
@@ -24,26 +24,9 @@ class FormManager_Field_Text extends FormManager_Field_String {
 	 */
 	public function __construct() {
 		parent::__construct();
-		$this->setDecorator('template', 'text');
-		// TODO реализовать базовую инициализацию элемента
+		$this
+			->addDecorator('template', 'text')
+			->addFilter(new FormManager_Filter_NotNull());
 	}
-
-	/**
-	 * Устанавливает фильтр для поля
-	 * 
-	 * @param string $name   Имя фильтра
-	 * @param array  $params Параметры фильтра
-	 * 
-	 * @return FormManager_Field_Text
-	 *//*
-	public function setFilter($name, $params = null) {
-		// TODO теперь подругому работает
-		if ($name == 'length' && (isset($params['max']) || isset($params['eq']))) {
-			$this->setViewParams(array(
-				'length' => isset($params['max']) ? $params['max'] : $params['eq']
-			));
-		}
-		return parent::setFilter($name, $params);
-	}*/
 
 }
