f3fa3ce3cd53ddd65c004f58d31dd62dcc437eed
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2012-02-08 20:13:46 +0000

    разбил списки сообщений на 3 группы: вид, ошибки, фильтры

---
+++ branches/form-manager_4.0/languages/en/error/.default.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * FormManager package
+ * 
+ * @package   FormManager
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @version   4.0 SVN: $Revision: 214 $
+ * @since     $Date: 2012-01-22 20:44:25 +0400 (Sun, 22 Jan 2012) $
+ * @link      http://peter-gribanov.ru/open-source/form-manager/4.0/
+ * @copyright 2008 by Peter Gribanov
+ * @license   http://peter-gribanov.ru/license	GNU GPL Version 3
+ */
+
+return array(
+	// Сообщения исключений
+// TODO нехватает кодов исключений
+	'error:000' => 'Unknown error',
+	'error:200' => 'Error in the model form',
+	'error:300' => 'Error in the model collections',
+	'error:400' => 'Error in the model questions',
+	'error:500' => 'Error in the model fields',
+	'error:600' => 'error filter',
+	'error:700' => 'Error in the database',
+	'error:701' => 'Name of driver of a database should be a string',
+	'error:702' => 'The driver class of the database is not responding interface "FormManager_Db_Interface"',
+);
---
+++ branches/form-manager_4.0/languages/ru/viwe/.default.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * FormManager package
+ * 
+ * @package   FormManager
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @version   4.0 SVN: $Revision: 214 $
+ * @since     $Date: 2012-01-22 20:44:25 +0400 (Sun, 22 Jan 2012) $
+ * @link      http://peter-gribanov.ru/open-source/form-manager/4.0/
+ * @copyright 2008 by Peter Gribanov
+ * @license   http://peter-gribanov.ru/license	GNU GPL Version 3
+ */
+
+return array(
+	// Шаблонные сообщения
+	'viwe:default_submit_title' => 'Отправить',
+	'viwe:field_yesno_yes'      => 'Да',
+	'viwe:field_yesno_no'       => 'Нет',
+);
