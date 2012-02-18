564d3ca7e624baaca095571fbee5ffea04871e2c
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2012-02-18 14:08:59 +0000

    перенес шаблон формы в подпапку form

--- templates/.default/form/template.php
+++ templates/.default/form/template.php
@@ -1,16 +1,4 @@
-<?
-/**
- * FormManager package
- * 
- * @package   FormManager
- * @author    Peter Gribanov <info@peter-gribanov.ru>
- * @version   4.0 SVN: $Revision$
- * @since     $Date$
- * @link      http://peter-gribanov.ru/open-source/form-manager/4.0/
- * @copyright 2008 by Peter Gribanov
- * @license   http://peter-gribanov.ru/license	GNU GPL Version 3
- */
-?><?if($this->getName()):?><div id="form-<?=$this->getName()?>">
+<?if($this->getName()):?><div id="form-<?=$this->getName()?>">
 <?endif?>
 <form action="<?=$this->getAction()?>" method="<?=$this->getMethod()?>"<?=
 ($this->getName() ? ' name="'.$this->getName().'"' : '')?> enctype="multipart/form-data">
