5dbe5f762eab965a63cda7b7cf547e272d7eea74
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2012-02-18 11:17:32 +0000

    добавил базовые шаблоны для таких частых операций как вывод списка ошибок или вывой лайбла

---
+++ branches/form-manager_4.0/templates/.default/_info.php
@@ -0,0 +1,24 @@
+<?
+/**
+ * Сообщение c информацией
+ * 
+ * @param array   $element    Все ниже описанное в ассоциативном массиве
+ * @param string  $name       Имя элемента
+ * @param boolean $valid      Элемент валиден
+ * @param boolean $changed    Элемент изменен
+ * @param string  $fullname   Полное имя элемента в форме 
+ * @param array   $filters    Фильтры [string,..]
+ * @param array   $decorators Декораторы {'id':string,'template':string,'label':string,'idseparator':',','options':{'key':'value'}}
+ * @param array   $errors     Ошибки [string,..]
+ * @param muxed   $value      Значение
+ * @param muxed   $default    Значение по умолчанию
+ * @param muxed   $input      Значение отправленное пользователем
+ */
+?>
+<?if(!empty($decorators['help'])):?>
+	<ul class="f-message-info">
+		<?foreach($decorators['help'] as $message):?>
+			<li><?=$message?></li>
+		<?endforeach;?>
+	</ul>
+<?endif;?>
