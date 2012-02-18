fe5eaedf8505dc50142feb6056ee11634172c8bc
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2012-02-18 16:05:15 +0000

    закончил с разработкой инструментов для установки плагинов шаблонов и языковых тем

--- branches/form-manager_4.0/FormManager/Plugins/Template.php
+++ branches/form-manager_4.0/FormManager/Plugins/Template.php
@@ -28,24 +28,27 @@ class FormManager_Plugins_Template implements FormManager_Plugins_Interface {
 	/**
 	 * Устанавливает шаблон
 	 * 
-	 * @param string $name  Имя шаблона
+	 * Проверяет установлен ли шаблон
+	 * 
+	 * @param string $name  Имя элемента для устанавливаемого шаблона
 	 * @param string $group Имя группы шаблонов
 	 * 
 	 * @return boolean
 	 */
 	static public function install($name, $group = FormManager_Template::DEFAULT_TEMPLATE) {
-		$name = strtolower($name);
-		if (self::isInstalled($name, $group)) {
-			return true; // ???
+		$name  = strtolower($name);
+		$group = strtolower($group);
+		// создаем пустой шаблон если его нет
+		if (!self::isInstalled($name, $group)) {
+			return file_put_contents(FORM_MANAGER_TEMPLATES_PATH.'/'.$group.'/'.$name.'/template.php', '');
 		}
-		// TODO требуется реализация
 		return true;
 	}
 
 	/**
 	 * Удаляет шаблон
 	 * 
-	 * @param string      $name  Имя шаблона
+	 * @param string      $name  Имя элемента для устанавливаемого шаблона
 	 * @param string|null $group Имя группы шаблонов. Если не укзано удаляет во всех группах
 	 * 
 	 * @return boolean
@@ -53,23 +56,32 @@ class FormManager_Plugins_Template implements FormManager_Plugins_Interface {
 	static public function uninstall($name, $group = null) {
 		$name = strtolower($name);
 		if (!self::isInstalled($name, $group)) {
-			return true; // ???
+			throw new FormManager_Exceptions_Logic(); // TODO описать исключение
 		}
-		// TODO требуется реализация
+		if ($group) {
+			self::rmdir(FORM_MANAGER_TEMPLATES_PATH.strtolower($group).'/'.$name);
+		} else {
+			// удаляем шаблон везде
+			$handler = dir(FORM_MANAGER_TEMPLATES_PATH);
+			while (false !== ($item = $handler->read())) {
+				if ($item != '.' && $item != '..' && is_dir(FORM_MANAGER_TEMPLATES_PATH.'/'.$item)) {
+					self::rmdir(FORM_MANAGER_TEMPLATES_PATH.'/'.$item.'/'.$name);
+				}
+			}
+		}
 		return true;
 	}
 
 	/**
 	 * Проверяет установлена ли шаблон
 	 * 
-	 * @param string $name  Имя шаблона
+	 * @param string $name  Имя элемента для устанавливаемого шаблона
 	 * @param string $group Имя группы шаблонов
 	 * 
 	 * @return boolean
 	 */
 	static public function isInstalled($name, $group = FormManager_Template::DEFAULT_TEMPLATE) {
-		// TODO требуется реализация
-		return false;
+		return file_exists(FORM_MANAGER_TEMPLATES_PATH.'/'.$group.'/'.$name.'/template.php');
 	}
 
 	/**
@@ -80,8 +92,37 @@ class FormManager_Plugins_Template implements FormManager_Plugins_Interface {
 	 * @return array
 	 */
 	static public function getListOfInstalled($group = FormManager_Template::DEFAULT_TEMPLATE) {
-		// TODO требуется реализация
-		return array();
+		$list = array();
+		$dir = FORM_MANAGER_TEMPLATES_PATH.'/'.$group.'/';
+		$handler = dir($dir);
+		while (false !== ($item = $handler->read())) {
+			if ($item != '.' && $item != '..' && file_exists($dir.$item.'/template.php')) {
+				$list[] = $item;
+			}
+		}
+		return $list;
+	}
+
+	/**
+	 * Рекурсивно удаляет директорию
+	 * 
+	 * @param string $dir Имя директории
+	 */
+	static private function rmdir($dir) {
+		if (!is_dir($dir)) {
+			return;
+		}
+		foreach (scandir($dir) as $item) {
+			if ($item == '.' || $item == '..') {
+				continue;
+			}
+			if (is_dir($dir.'/'.$item)) {
+				self::rmdir($dir.'/'.$item);
+			} else {
+				unlink($dir.'/'.$item);
+			}
+		}
+		rmdir($dir);
 	}
 
 }
