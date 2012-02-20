8761c16431a9c2b07f6789c73623fa31fa768c6f
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2012-02-20 21:29:25 +0000

    доделал хелперы для шаблонов. исправил те что работали неправильно и удалил те которые ненужны

--- FormManager/Template.php
+++ FormManager/Template.php
@@ -24,7 +24,14 @@ class FormManager_Template {
 	 * 
 	 * @var string
 	 */
-	const DEFAULT_TEMPLATE = '.default';
+	const DEFAULT_TEMPLATE = '.default'; // TODO протестировать на php 5.1
+
+	/**
+	 * Префикс для ключа языковых сообщений
+	 * 
+	 * @var string
+	 */
+	const MESSAGE_PREFIX = 'view:';
 
 
 	/**
@@ -32,57 +39,35 @@ class FormManager_Template {
 	 * 
 	 * @see FormManager_Language::getMessage()
 	 * 
-	 * @param string $id     Id сообщения
-	 * @param array  $params Параметры сообщения
+	 * @param string      $id     Идентификатор сообщения
+	 * @param array       $params Параметры сообщения
+	 * @param string|null $group  Имя группы сообщений
 	 * 
 	 * @return string
 	 */
-	public function lang($id, array $params = array()) {
-		return FormManager_Language::getMessage('viwe:'.$id, $params);
+	public static function lang($id, array $params = array(), $group = null) {
+		return FormManager_Language::getMessage(self::MESSAGE_PREFIX.$id, $params, $group);
 	}
 
 	/**
-	 * Хелпер возвращающий абсолютный HTTP путь
+	 * Хелпер проверяющий и возвращающий путь к шаблону
 	 * 
-	 * @param string $path Путь отнасительно корня шаблона
+	 * @param string $path Путь к шаблону (dir/to/template.php)
 	 * 
-	 * @return string
+	 * @return string|boolean
 	 */
-	public function path($path) {
-		if (file_exists(FORM_MANAGER_PATH.'templates/'.$this->template.$path)) {
-			return FORM_MANAGER_HTTP_PATH.'templates/'.$this->template.$path;
+	public static function path($path) {
+		if (file_exists(FORM_MANAGER_TEMPLATES_PATH.$path)) {
+			return $path;
+		} else {
+			// если выбранный шаблон не существует пытаемся получить путь в шаблоне по умолчанию
+			$path = str_replace('\\', '/', substr($path, 1));
+			if (strpos($path, '/') !== false) {
+				list(, $path) = explode('/', $path, 2);
+				return self::DEFAULT_TEMPLATE.$path;
+			}
 		}
-		return FORM_MANAGER_HTTP_PATH.'templates/'.self::DEFAULT_TEMPLATE.'/'.$path;
-	}
-
-	/**
-	 * Хелпер включающий шаблон поля
-	 * 
-	 * @param string $type Название поля
-	 * @param array  $vars Переменные передаваемые в шаблон
-	 *//*
-	public function field($type, array $vars = array()) {
-		$this->inc('fields/'.$type.'/template.php', $vars);
-	}*/
-
-	/**
-	 * Хелпер включающий шаблон колекции
-	 * 
-	 * @param string $type Название колекции
-	 * @param array  $vars Переменные передаваемые в шаблон
-	 *//*
-	public function collection($type, array $vars = array()) {
-		$this->inc('collections/'.$type.'/template.php', $vars);
-	}*/
-
-	/**
-	 * Хелпер включающий шаблон элемента
-	 * 
-	 * @param string $type Название колекции
-	 * @param array  $vars Переменные передаваемые в шаблон
-	 */
-	public function element($type, array $vars = array()) {
-		$this->inc($type.'/template.php', $vars);
+		return false;
 	}
 
 	/**
@@ -91,37 +76,22 @@ class FormManager_Template {
 	 * @param string $template Адрес шаблона
 	 * @param array  $vars     Переменные передаваемые в шаблон
 	 */
-	public function inc($template, array $vars = array()) {
+	public static function inc($template, array $vars = array()) {
 		extract($vars, EXTR_SKIP | EXTR_REFS);
-		include $this->path($template);
+		//$helper = $this;
+		include FORM_MANAGER_TEMPLATES_PATH.self::path($template);
 	}
 
 	/**
-	 * Возвращающий абсолютный путь
+	 * Хелпер включающий шаблон элемента формы
 	 * 
-	 * @throws FormManager_Exceptions_InvalidArgument
-	 * 
-	 * @param string $path Путь отнасительно корня шаблона
-	 * 
-	 * @return string
-	 *//*
-	private function getLocalPath($path) {
-		if (!is_string($path) || !trim($path)) {
-			// TODO описать исключение
-			throw new FormManager_Exceptions_InvalidArgument();
-		}
-		if (file_exists(FORM_MANAGER_PATH.'templates/'.$this->template.'/'.$path)) {
-			return FORM_MANAGER_PATH.'templates/'.$this->template.'/'.$path;
-		}
-		return FORM_MANAGER_PATH.'templates/'.self::DEFAULT_TEMPLATE.'/'.$path;
-	}*/
-
-	/**
-	 * Рисует форму
-	 * 
-	 * @param FormManager_Form $form Форма
+	 * @param array $element Описание элемента формы
 	 */
-	public function drow(FormManager_Form $form) {
-		$this->inc('template.php', $form->assemble());
+	public static function form(array $element) {
+		if (isset($element['decorators']['template'])) {
+			$element['element'] = $element;
+			self::inc($element['decorators']['template'], $element);
+		}
 	}
+
 }
