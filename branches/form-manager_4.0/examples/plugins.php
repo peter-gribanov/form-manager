2d497b1c4622519a2d4734056b64d80774323383
wcode404@gmail.com <wcode404@gmail.com@8577a3a6-9e72-91f8-f643-eb138b0ee71e>
2012-02-20 21:27:22 +0000

    сделал пример по инсталяции и удалению плагинов

--- branches/form-manager_4.0/examples/plugins.php
+++ branches/form-manager_4.0/examples/plugins.php
@@ -1,14 +1,49 @@
 <?php
 include('../init.php');
 
-// return true
-p(FormManager_Plugins::install('test'));
+echo 'Is installed';
+p(FormManager_Plugins::isInstalled('Test'));
 
-// return true
-p(FormManager_Plugins::isInstalled('test'));
+echo 'Install';
+p(FormManager_Plugins::install('Test'));
+
+echo 'Is installed<br />';
+echo 'Use of plug-in can be on the next request';
+p(FormManager_Plugins::isInstalled('Test'));
 
 // return list of all installed elements
+echo 'List of installed';
 p(FormManager_Plugins::getListOfInstalled());
 
-// return true
-p(FormManager_Plugins::uninstall('test'));
+echo 'Uninstall';
+p(FormManager_Plugins::uninstall('Test'));
+
+echo 'Is installed';
+p(FormManager_Plugins::isInstalled('Test'));
+
+echo 'Install all filters<br />';
+
+installFilters(FORM_MANAGER_PATH.'/FormManager/Filter/');
+
+
+/**
+ * Install all filters
+ * 
+ * @param string $dir The path to the directory with the filters
+ */
+function installFilters($dir, $subdir = '') {
+	$exception =  array('.', '..', 'Abstract.php', 'Interface.php', 'Factory.php', 'Builder.php');
+	foreach (scandir($dir.$subdir) as $item) {
+		if (in_array($item, $exception)) {
+			continue;
+
+		} elseif (is_file($dir.$subdir.$item)) {
+			$filter = str_replace('/', '_', $subdir).pathinfo($item, PATHINFO_FILENAME);
+			echo 'Instal filter '.$filter.'<br />';
+			FormManager_Plugins_Filter::install($filter);
+
+		} elseif (is_dir($dir.$subdir.$item)) {
+			installFilters($dir, $subdir.$item.'/');
+		}
+	}
+}
